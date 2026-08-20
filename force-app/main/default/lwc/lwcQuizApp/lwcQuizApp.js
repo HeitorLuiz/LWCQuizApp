b65633991cb8eba084d482fa2a7f055a0921ca98
Heitor <heitor.2100@gmail.com>
2026-08-20 19:57:02 -0300

    Continuação do projeto LWC Quiz App

--- force-app/main/default/lwc/lwcQuizApp/lwcQuizApp.js
+++ force-app/main/default/lwc/lwcQuizApp/lwcQuizApp.js
@@ -1,3 +1,38 @@
 import { LightningElement } from 'lwc';
 
-export default class LwcQuizApp extends LightningElement {}
+export default class LwcQuizApp extends LightningElement {
+
+    myQuestions= [{
+        id: "Question1",
+        question: "Which one of the following is not a template loop?",
+        answers:{
+            a: "foreach",
+            b: "iterator",
+            c: "map loop"
+            },
+        correctAnswer: "c"
+            },{
+        id: "Question2",
+        question: "Which of the file is invalid in LWC component folder?",
+        answers:{
+            a: ".svg",
+            b: ".apex",
+            c: ".js"
+        },
+        correctAnswer: "b"
+    },{
+        id: "Question3",
+        question: "Which method is used to navigate in LWC?",
+        answers:{
+            a: "NavigationMixin",
+            b: "navigateTo",
+            c: "navigate"
+        },
+        correctAnswer: "a"
+    }
+                ]
+                changeHandler(event) {
+                    console.log("name", event.target.name);
+                    console.log("value", event.target.value);
+    }
+}
