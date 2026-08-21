2bbc9f785e2bc7ad60426fb3cc0ed4d7a93f489c
Heitor <heitor.2100@gmail.com>
2026-08-21 00:11:03 -0300

    LWC Quiz App funcionando

--- force-app/main/default/lwc/lwcQuizApp/lwcQuizApp.js
+++ force-app/main/default/lwc/lwcQuizApp/lwcQuizApp.js
@@ -2,6 +2,9 @@ import { LightningElement } from 'lwc';
 
 export default class LwcQuizApp extends LightningElement {
 
+    selected={}
+    correctAnswers = 0
+    isSubmitted = false
     myQuestions= [{
         id: "Question1",
         question: "Which one of the following is not a template loop?",
@@ -31,8 +34,28 @@ export default class LwcQuizApp extends LightningElement {
         correctAnswer: "a"
     }
                 ]
+                get allNotSelected(){
+                    return !(Object.keys(this.selected).length === this.myQuestions.length)
+                }
+                get isScoredFull(){
+                    return `slds-text-heading_large ${this.myQuestions.length === this.correctAnswers ? 'slds-text-color_success' : 'slds-text-color_error' }`
+                }
                 changeHandler(event) {
                     console.log("name", event.target.name);
                     console.log("value", event.target.value);
+                    const {name, value} = event.target;
+                    this.selected = {...this.selected, [name]: value};
     }
+                submitHandler(event){
+                    event.preventDefault()
+                    let correct = this.myQuestions.filter(item=>this.selected[item.id] === item.correctAnswer)
+                    this.correctAnswers = correct.length
+                    this.isSubmitted = true
+                    console.log("this.correctAnswers", this.correctAnswers)
+                }
+                resetHandler(){
+                    this.selected = {}
+                    this.correctAnswers = 0
+                    this.isSubmitted = false
+                }
 }
